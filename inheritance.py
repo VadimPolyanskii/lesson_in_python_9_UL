945219cbf6cb26fc065226ed4186b7b8fab034cb
Vad Pol <work.arb19@gmail.com>
2022-02-18 19:26:54 +0300

    Наследование

---
+++ inheritance.py
@@ -0,0 +1,40 @@
+# Импортируем родительский класс
+from new_class import NumberCalculation
+
+
+# Наследственный класс
+class NaturalNumberTest(NumberCalculation):
+    '''
+    Проверим, являются ли результирующие числа целыми
+    '''
+    # Инициализация
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    # Произведение чисел
+    def product_numbers(self):
+        return self._x * self._y
+
+    def number_check(self):
+        d = 2
+        while self._x * self._y % d != 0:
+            d += 1
+        return d == self._x * self._y
+
+
+if __name__ == '__main__':
+    number = NaturalNumberTest(15, 16)
+    print()
+    print('Числа', number._x, number._y)
+    print('Призведение чисел', number._x, 'и', number._y, 'равно', number._x * number._y)
+    print('Является ли число', number._x * number._y, 'простым?', number.number_check())
+
+
+
+
+
+
+
+    
+
+
