5eedb7a58ed87d5d4b7fbb2f516b8078bf465c56
Vad Pol <work.arb19@gmail.com>
2022-08-08 19:21:29 +0300

    Инкапсуляция

--- new_class.py
+++ new_class.py
@@ -20,15 +20,31 @@ class NumberCalculation:
         self._x = random.randint(0, 20)
         self._y = random.randint(0, 20)
 
-    # Получим доступ к инкапсулированному значению x
+    # Получим доступ к инкапсулированному значению x через декоратор доступа
     @property
     def get_x(self):
         return self._x
 
-    # Получим доступ к инкапсулированному значению y
+    # Перенастроим число x, используя декоратор настройки
+    @get_x.setter
+    def get_x(self, x):
+        if (x > 0) & (x < 20):
+            self._x = x
+        else:
+            raise ValueError('Числа должны быть от 0 до 20!')
+
+    # Получим доступ к инкапсулированному значению y через декоратор доступа
     @property
     def get_y(self):
         return self._y
+
+    # Перенастроим число x, используя декоратор настройки
+    @get_y.setter
+    def get_y(self, y):
+        if (y > 0) & (y < 20):
+            self._y = y
+        else:
+            raise ValueError('Число должно быть от 0 до 20')
 
     # Произведение чисел
     def product_numbers(self):
@@ -61,6 +77,10 @@ if __name__ == '__main__':
     # Создадим объект-экземпляр класса
     obj = NumberCalculation(5, 5)
     obj.arbitrary_numbers()     # настроим числа
+
+    # Перенастроим числа
+    obj.get_x = -2
+    obj.get_y = 43
     # Выведем объект с атрибутами класса и результирующее число
     print('Числа:', obj.get_x, obj.get_y)
     print('Произведение чисел', obj.get_x, 'и', obj.get_y, 'равно', obj.product_numbers())
